6f92e7b592e0cf645362790ff5a7f50552719c43
prakash-indorkar <60499738+prakash-indorkar@users.noreply.github.com>
2024-09-05 06:20:25 +0000

    Test rg

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-# Configure the Microsoft Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
+  subscription_id = "146c07c5-5543-41fb-8f52-c697768ac9da"
 }
 
-# Create a Resource Group if it doesn’t exist
-resource "azurerm_resource_group" "tfexample" {
-  name     = "my-terraform-rg"
+resource "azurerm_resource_group" "this" {
   location = "Australia East"
-}
-
-# Create a Virtual Network
-resource "azurerm_virtual_network" "tfexample" {
-  name                = "my-terraform-vnet"
-  location            = azurerm_resource_group.tfexample.location
-  resource_group_name = azurerm_resource_group.tfexample.name
-  address_space       = ["10.0.0.0/16"]
+  name     = "sample-resource-group1"
 }
